5746abe117a4ae7e79a944fb4932c3f061ee53ad
pachgadehardik <pachgadehardik@gmail.com>
2020-10-07 22:06:47 +0530

    Added Insert after given node feature

--- src/test/java/hardik/data_structure/LinkedListTest.java
+++ src/test/java/hardik/data_structure/LinkedListTest.java
@@ -3,30 +3,27 @@ package hardik.data_structure;
 import org.junit.*;
 
 public class LinkedListTest {
-	
-	
+
 	LinkedList list;
-	
+
 	@Before
 	public void init() {
-		 list = new LinkedList();
-		 list = LinkedList.insert(list,56);
-	     list = LinkedList.insert(list,30);
-	     list = LinkedList.insert(list,70);
+		list = new LinkedList();
+		list = LinkedList.insert(list, 56);
+		list = LinkedList.insert(list, 30);
+		list = LinkedList.insert(list, 70);
 	}
-	
+
 	@Test
 	public void getSearchElementShouldReturnTrue() {
-    
+
 		Assert.assertEquals(30, LinkedList.search(list, 30).getKey());
-    	
+
 	}
-	
+
 	@Test
 	public void getProperListInsertingAfterGivenElement() {
-		
-		
-		Assert.assertTrue(LinkedList.insertAfterNode(list, 50, 56));		
+		Assert.assertTrue(LinkedList.insertAfterNode(list, 50, 56));
 	}
-	
+
 }
